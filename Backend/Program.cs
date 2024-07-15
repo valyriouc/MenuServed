44ee8beaa9acb7ee6567c3129b4e41fcb1af4cfa
Valacor <valyrious.informatics@gmail.com>
2024-07-15 20:50:55 +0200

    Add JWT authentication

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,17 +1,48 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+using Backend.Helpers;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = "Bearer";
+            options.DefaultChallengeScheme = "Bearer";
+        })
+        .AddJwtBearer("Bearer", options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secrets.Instance.Secret)), // TODO: Change secret
+                ValidIssuer = Secrets.Instance.Issuer,
+                ValidAudience = Secrets.Instance.Audience,
+            };
+        });
+
+        WebApplication app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.Run();
