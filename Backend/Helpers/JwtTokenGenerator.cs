13411f0b37e236aca87393b814acc08a3899443f
Valacor <valyrious.informatics@gmail.com>
2024-07-15 20:53:25 +0200

    Token generation & secret handling

---
+++ Backend/Helpers/JwtTokenGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Backend.Helpers;
+
+public class JwtTokenGenerator
+{
+    private readonly SymmetricSecurityKey _key;
+
+    public JwtTokenGenerator(string secretKey)
+    {
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secrets.Instance.Secret));
+    }
+
+    public string GenerateToken(string username, string role)
+    {
+        JwtSecurityToken token = new JwtSecurityToken(
+            issuer: Secrets.Instance.Issuer,
+            audience: Secrets.Instance.Audience,
+            claims: new[] { new Claim(ClaimTypes.Name, username), new Claim(ClaimTypes.Role, role) },
+            expires: DateTime.UtcNow.AddMinutes(30),
+            signingCredentials: new SigningCredentials(_key, SecurityAlgorithms.HmacSha256)
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ Backend/Helpers/Secrets.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace Backend.Helpers;
+
+internal struct Secrets
+{
+    public string Secret { get; set; }
+
+    public string Issuer { get; set; }
+
+    public string Audience { get; set; }    
+
+    private static Secrets? _instance = null;
+
+    public static Secrets Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Secrets.FromFile("secrets.json");
+            }
+
+            return _instance ?? throw new Exception();
+        }
+    }
+
+    private static Secrets FromFile(string file) => JsonSerializer.Deserialize<Secrets>(file);
+}
