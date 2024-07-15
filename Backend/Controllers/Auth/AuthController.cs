f10ef8ed156f2702e45423b304c37548808c0307
Valacor <valyrious.informatics@gmail.com>
2024-07-15 21:03:56 +0200

    Controller for auth stuff

---
+++ Backend/Controllers/Auth/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Backend.Helpers;
+using Backend.Transfer.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers.Auth;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly JwtTokenGenerator _tokenGenerator;
+
+    public AuthController(JwtTokenGenerator tokenGenerator)
+    {
+        _tokenGenerator = tokenGenerator;
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public IActionResult Login([FromBody] LoginData data)
+    {
+       
+        return Ok();
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public IActionResult Register([FromBody] RegisterData data)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Logout()
+    {
+
+        return Ok(); 
+    }
+}
