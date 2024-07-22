8a14976bcbc98c0319410964dccc85cd52d48785
Valacor <valyrious.informatics@gmail.com>
2024-07-23 00:06:44 +0200

    Make data annotations for the user model

--- Backend/Models/UserModel.cs
+++ Backend/Models/UserModel.cs
@@ -1,9 +1,26 @@
-﻿namespace Backend.Models;
+﻿using Backend.Helpers;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models;
 
 public enum UserRole
 {
     Customer,
     Restaurent
+}
+
+/// <summary>
+/// QUESTION: Is this the right method to make money with it???
+/// - Prcing per click 
+/// - Pricing per order 
+/// - Pricing with different features 
+/// </summary>
+public enum Pricing
+{
+    FreeTrial,
+    Customer,
+    Restaurent,
 }
 
 /// <summary>
@@ -15,17 +32,46 @@ public class Filter
 }
 
 /// <summary>
-/// The basic user model
+/// QUESTION: What needs to be done here?
 /// </summary>
+public class PaymentMethod
+{
+    public uint Id { get; set; }
+
+
+}
+
+/// <summary>
+/// The basic user model
+/// How do we make sure that we get paid 
+/// </summary>
+[Table("users")]
 public class UserModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = null!;
 
+    [Required]
+    [MinLength(ApiConfig.UsernameMinLength)]
+    [MaxLength(ApiConfig.UsernameMaxLength)]    
     public string UserName { get; set; } = null!;
 
+    [Required]
     public UserRole Role { get; set; }
 
+    // It's a SHA512 hash
+    [Required]
     public string Password { get; set; } = null!;
+
+    /// <summary>
+    /// Pricing section 
+    /// </summary>
+    public Pricing PricingPlan { get; set; }
+
+    public DateTime? FreeTrialStarted { get; set; } 
 }
