156729b9f38a40702d72fdc9e7cd2e9905115c0e
Valacor <valyrious.informatics@gmail.com>
2024-07-24 22:44:01 +0200

    Add a registration service

--- Backend/Services/Auth/LoginService.cs
+++ Backend/Services/Auth/LoginService.cs
@@ -1,21 +1,38 @@
 ﻿
+using Backend.Database;
+using Backend.Information;
+using Backend.Models;
 using Backend.Transfer.Auth;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Services.Auth;
 
-public class LoginService : IServiceBase<LoginData, LoginResponse>
+public class LoginService : IServiceBase<LoginData, AuthResponse>
 {
-    public ILogger Logger { get; } 
+    private AppDbContext DbContext { get; }
+
+    public ILogger Logger { get; }
 
     public LoginService(
+        AppDbContext dbContext,
         ILogger<LoginService> logger)
     {
+        DbContext = dbContext;
         Logger = logger;
     }
 
-    public Task<LoginResponse> RunAsync(LoginData data)
+    public async Task<AuthResponse> RunAsync(LoginData data)
     {
         // Query database for specified user 
+        UserModel? user = await DbContext.Users.FirstOrDefaultAsync(employee => employee.Email == data.Email);
+        
+        if (user is null)
+        {
+            throw ApiException.NotFound(
+                "User does not exist!");
+        }
+
+        throw new NotImplementedException();
 
         // Check if user exists 
 
---
+++ Backend/Services/Auth/RegisterService.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Transfer.Auth;
+
+namespace Backend.Services.Auth;
+
+public class RegisterService : IServiceBase<RegisterData, AuthResponse>
+{
+    public ILogger Logger { get; set; }
+
+    public bool IsCustomerRegistration { get; set; } = true;
+
+    public RegisterService(ILogger logger)
+    {
+        Logger = logger;
+    }
+
+    public Task<AuthResponse> RunAsync(RegisterData data)
+    {
+        throw new NotImplementedException();
+    }
+}
