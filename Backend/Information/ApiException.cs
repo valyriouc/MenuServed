02c539e0e1b50b8275f7227e7153d2f51ef09177
Valacor <valyrious.informatics@gmail.com>
2024-07-24 22:45:26 +0200

    Starting with an information system

---
+++ Backend/Information/ApiException.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace Backend.Information;
+
+public class ApiException : Exception
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public ApiException(HttpStatusCode statusCode, string message) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public static ApiException NotFound(string message="") => 
+        new ApiException(HttpStatusCode.NotFound, message);
+
+    public static ApiException BadRequest(string message="") =>
+        new ApiException(HttpStatusCode.BadRequest, message);
+
+    public static ApiException Unauthorized(string message="") => 
+        new ApiException(HttpStatusCode.Unauthorized, message);
+}
---
+++ Backend/Information/InformationData.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace Backend.Information;
+
+public struct InformationMessage
+{
+    public string Content { get; set; }
+}
+
+public struct InformationData
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public IEnumerable<InformationMessage> Messages { get; } 
+
+    public InformationData(HttpStatusCode statusCode) 
+        : this(
+              statusCode, 
+              Enumerable.Empty<InformationMessage>()) { }
+
+    public InformationData(
+        HttpStatusCode statusCode, 
+        IEnumerable<InformationMessage> messages) 
+    { 
+        StatusCode = statusCode; 
+        Messages = messages;
+    }
+}
